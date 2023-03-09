66d502e81e4cba4e362306a8a97d11106f2c5655
quand <quand@DEATH>
2023-03-09 16:16:27 +0700

    Add project files.

---
+++ BaiThucHanh3/Controllers/CoursesController.cs
@@ -0,0 +1,38 @@
+﻿using BaiThucHanh3.Models;
+using BaiThucHanh3.ViewModels;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BaiThucHanh3.Controllers
+{
+    public class CoursesController : Controller
+    {
+        // GET: Courses
+        private readonly ApplicationDbContext _dbContext;
+        private object Categories;
+
+        public CoursesController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+        //[Authorize]
+        [HttpPost]
+        public ActionResult Create(CourseViewModel viewModel)
+        {
+            var courses = new Course
+            {
+                LecturerId = User.Identity.GetUserId(),
+                DateTime = viewModel.GetDateTime(),
+                CategoryId= viewModel.Category,
+                Plance = viewModel.Place
+            };
+            _dbContext.Courses.Add(courses);
+            _dbContext.SaveChanges();
+            return RedirectToAction("Index","Home");
+        }
+    }
+}
---
+++ BaiThucHanh3/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BaiThucHanh3.Startup))]
+namespace BaiThucHanh3
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
